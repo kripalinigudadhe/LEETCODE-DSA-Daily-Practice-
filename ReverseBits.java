20876658fefd65fe83baaf60945b2a5174747bde
Kripalini Gudadhe <kripalinigudadhe3@gmail.com>
2026-02-16 22:51:55 +0530

    Add files via upload

---
+++ ReverseBits.java
@@ -0,0 +1,22 @@
+public class ReverseBits {
+
+    public static void main(String[] args) {
+
+        int n = 43261596;   // Example input
+        System.out.println(reverseBits(n));
+    }
+
+    public static int reverseBits(int n) {
+
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+
+            result <<= 1;          // Shift result left
+            result |= (n & 1);     // Add last bit of n
+            n >>= 1;               // Shift n right
+        }
+
+        return result;
+    }
+}

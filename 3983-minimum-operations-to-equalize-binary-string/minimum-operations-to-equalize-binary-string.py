59fff1699cfb9e12007d686a75e0c80acfc3c5c7
Kripalini Gudadhe <kripalinigudadhe3@gmail.com>
2026-03-04 13:13:32 +0530

    Time: 127 ms (46.61%) | Memory: 16.1 MB (52.28%) - LeetSync

---
+++ 3983-minimum-operations-to-equalize-binary-string/minimum-operations-to-equalize-binary-string.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minOperations(self, s, k):
+        n = len(s)
+
+        z = 0
+        o = 0
+
+        for c in s:
+            if c == '0':
+                z += 1
+            else:
+                o += 1
+
+        if z == 0:
+            return 0
+
+        for op in range(1, n + 1):
+            flips = op * k
+
+            if (flips - z) % 2 == 1:
+                continue
+
+            if op % 2 == 0:
+                if z <= flips <= z * (op - 1) + o * op:
+                    return op
+            else:
+                if z <= flips <= z * op + o * (op - 1):
+                    return op
+
+        return -1
